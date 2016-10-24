842504ab9ccf2235c9872b277463f8b70aab10bb
Kristis <teniauskas.kristis@gmail.com>
2016-10-24 16:31:02 +0100

    Fixed email form

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,6 +5,14 @@ class StaticPagesController < ApplicationController
   def landing_page
   @products = Product.limit(6)
   end
+
+  #Email
+  def thank_you
+  @name = params[:name]
+  @email = params[:email]
+  @message = params[:message]
+  UserMailer.contact_form(@email, @name, @message).deliver_now
+  end
 end
 
 	# Email in developer mode
@@ -18,11 +26,4 @@ end
   #    :body => @message).deliver_now
 #end
 
-	#Email
-	def thank_you
-  @name = params[:name]
-  @email = params[:email]
-  @message = params[:message]
-  UserMailer.contact_form(@email, @name, @message).deliver_now
-end
 
