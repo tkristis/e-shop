efec7a6aa999b13327c1d02be9901cba775937f5
Kristis <teniauskas.kristis@gmail.com>
2016-10-31 21:21:52 +0000

    Fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161028201321) do
-
-  create_table "comments", force: :cascade do |t|
-    t.integer  "user_id"
-    t.text     "body"
-    t.integer  "rating"
-    t.integer  "product_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["product_id"], name: "index_comments_on_product_id"
-    t.index ["user_id"], name: "index_comments_on_user_id"
-  end
+ActiveRecord::Schema.define(version: 20161028163647) do
 
   create_table "object_names", force: :cascade do |t|
     t.datetime "created_at", null: false
