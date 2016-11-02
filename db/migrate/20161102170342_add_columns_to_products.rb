d704dc7217caf919b5fa19ff9a548d76b8ac5dd5
Kristis <teniauskas.kristis@gmail.com>
2016-11-02 17:08:09 +0000

    added products column

--- db/migrate/20161102170342_add_columns_to_products.rb
+++ db/migrate/20161102170342_add_columns_to_products.rb
@@ -1,5 +1,5 @@
 class AddColumnsToProducts < ActiveRecord::Migration[5.0]
   def change
-  	add_column :products, :Year_of_manufacture, :integer
+  	add_column :products, :year_of_manufacture, :integer
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161028163647) do
+ActiveRecord::Schema.define(version: 20161102170342) do
 
   create_table "object_names", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -33,7 +33,8 @@ ActiveRecord::Schema.define(version: 20161028163647) do
     t.datetime "updated_at",          null: false
     t.string   "color"
     t.integer  "price"
-    t.integer  "Year_of_manufacture"
+    t.integer  "year_or_manufacture"
+    t.integer  "year_of_manufacture"
   end
 
   create_table "users", force: :cascade do |t|
