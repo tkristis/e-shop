497ecfb540fe87f4b59dcf12f2ff0d68b73e6836
Kristis <teniauskas.kristis@gmail.com>
2016-11-08 01:34:35 +0000

    5.8 comment form and ratings

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,3 +1,10 @@
 $(document).on('turbolinks:load', function(){
-    $('.rating').raty( { path: '/assets/raty' });
+    $('.rating').raty( { path: '/assets/raty', scoreName: 'comment[rating]' 
+});
+	$('.rated').raty({ path: '/assets/raty',
+      readOnly: true,
+      score: function() {
+        return $(this).attr('data-score');
+      }
+    });
 });
--- app/models/product.rb
+++ app/models/product.rb
@@ -5,4 +5,8 @@ class Product < ActiveRecord::Base
   def highest_rating_comment
   	comments.rating_desc.first
 	end
+	
+	def average_rating
+  comments.average(:rating).to_f
+	end
 end
