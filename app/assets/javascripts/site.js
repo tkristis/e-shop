8a20d6ee6f4ea8e1ada7dc8a0951beddb33588b9
Kristis <teniauskas.kristis@gmail.com>
2016-12-02 18:21:59 +0000

    6.9: JavaScript on Rails

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,10 +1,18 @@
 $(document).on('turbolinks:load', function(){
-    $('.rating').raty( { path: '/assets/raty', scoreName: 'comment[rating]' 
+
+  // Product coment review stars 
+  $('.rating').raty( { path: '/assets/raty', scoreName: 'comment[rating]' 
 });
-	$('.rated').raty({ path: '/assets/raty',
-      readOnly: true,
-      score: function() {
-        return $(this).attr('data-score');
-      }
-    });
+  $('.rated').raty({ path: '/assets/raty',
+    readOnly: true,
+    score: function() {
+      return $(this).attr('data-score');
+    }
+  });
+
+  // Product image zoom 
+  $('.img-zoom').elevateZoom({
+    zoomType        : "inner",
+    cursor: "crosshair"
+  }); 
 });
