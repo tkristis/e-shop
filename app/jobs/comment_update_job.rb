f60dcd30f674ca6af77163f275fdb2760c5786c7
Kristis <teniauskas.kristis@gmail.com>
2016-12-19 16:46:08 +0000

    6.14: ActiveJob and Advanced ActionCable

--- app/jobs/comment_update_job.rb
+++ app/jobs/comment_update_job.rb
@@ -2,12 +2,12 @@ class CommentUpdateJob < ApplicationJob
   queue_as :default
 
   def perform(comment, current_user)
-    ProductChannel.broadcast_to(@product.id, comment: CommentsController.render(partial: 'comments/comment', locals: {comment: @comment, current_user: current_user }), average_rating: @product.average_rating)
+    ProductChannel.broadcast_to comment.product_id, comment: render_comment(comment, current_user)
   end
 
   private
 
   def render_comment(comment, current_user)
-    CommentsController.render(partial: 'comments/comment', locals: { comment: comment, current_user: current_user })
+    CommentsController.render(partial: 'comments/comment', locals: {comment: comment, current_user: current_user })
   end
 end
