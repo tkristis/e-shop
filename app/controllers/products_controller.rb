19ae59c9a85c64b87b6a5e4a086004fb36219b08
Kristis Teniauskas <teniauskas.kristis@gmail.com>
2017-03-28 04:13:19 +0100

    footer links, fixing products error

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -83,7 +83,7 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:name, :description, :image_url, :color, :price, :year_of_manufacture)
+      params.require(:product).permit(:name, :description, :image_url, :color, :price, :year_of_manufacture, :category_id)
     end
   end
 
