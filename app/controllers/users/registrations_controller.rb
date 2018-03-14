7cf7cb5fccbc7df1ae644678465df96ed4644623
Kristis <teniauskas.kristis@gmail.com>
2018-03-14 14:19:05 +0000

    added better errors gem and trying to fix recaptcha

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -11,19 +11,19 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # POST /resource
-  # def create
-  #  if !verify_recaptcha
-  #     flash.delete :recaptcha_error
-  #     build_resource(sign_up_params)
-  #     resource.valid?
-  #     resource.errors.add(:base, "There was an error with the recaptcha code below. Please re-enter the code.")
-  #     clean_up_passwords(resource)
-  #     respond_with_navigational(resource) { render_with_scope :new }
-  #   else
-  #     flash.delete :recaptcha_error
-  #     super
-  #   end
-  # end
+  def create
+   if !verify_recaptcha
+      flash.delete :recaptcha_error
+      build_resource(sign_up_params)
+      resource.valid?
+      resource.errors.add(:base, "There was an error with the recaptcha code below. Please re-enter the code.")
+      clean_up_passwords(resource)
+      respond_with_navigational(resource) { render_with_scope :new }
+    else
+      flash.delete :recaptcha_error
+      super
+    end
+  end
 
   # GET /resource/edit
   # def edit
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,7 +32,7 @@ Rails.application.configure do
     }
 
   # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local       = true
   config.action_controller.perform_caching = true
 
   # Disable serving static files from the `/public` folder by default since
